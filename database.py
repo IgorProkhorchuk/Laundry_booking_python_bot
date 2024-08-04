5470e014121b459236e1d7ef398d379b62b77fb1
Igor Prokhorchuk <igor@prokhorchuk.com>
2024-08-04 23:56:48 +0200

    fix: modify database initialisation to add user_id

--- database.py
+++ database.py
@@ -1,21 +1,36 @@
 import sqlite3
 
-def init_db():
+def create_booking_table():
+    """Creates the booking table if it doesn't exist."""
     conn = sqlite3.connect('bookings.db')
-    c = conn.cursor()
-    c.execute('''
-            CREATE TABLE IF NOT EXISTS booking (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                day TEXT,
-                slot TEXT,
-                name TEXT
-            )
-        ''')
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS booking (
+                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      day TEXT,
+                      slot TEXT,
+                      name TEXT,
+                      user_id INTEGER
+                      )""")
+    conn.commit()
+    conn.close()
+
+def create_user_table():
+    """Creates the user table if it doesn't exist."""
+    conn = sqlite3.connect('bookings.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      user_id INTEGER,
+                      username TEXT
+                      )""")
     conn.commit()
     conn.close()
 
 if __name__ == "__main__":
-    init_db()
+    create_booking_table()
+    create_user_table()
 
 weekdays = ["Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця"]
 timeslots = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00']
